e7ce02450e0266a5e65a69c1df3502d55d13e940
shehani-wijetunga <shehaniwijetunga96@gmail.com>
2021-02-10 23:35:02 +0530

    completed auth server 9th video project

---
+++ authorization-server/src/main/java/com/shehani/rentcloud/authorizationserver/model/Permission.java
@@ -0,0 +1,31 @@
+package com.shehani.rentcloud.authorizationserver.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "permission")
+
+public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
